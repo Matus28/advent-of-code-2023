e1b32bfefc3edb959f96c2426167647fca31a1fd
Matus Tatarko <matus.tatarko28@gmail.com>
2023-12-05 23:05:50 +0100

    refactor: Change logic, adds calculateSum and replaceSpecialCharWithStars function

--- src/days/day03/puzzle01/solution.ts
+++ src/days/day03/puzzle01/solution.ts
@@ -17,9 +17,46 @@ const readFile = (filename: string): string => {
   return content;
 };
 
+const replaceSpecialCharWithStars = (input: string): string => {
+  return input.replace(/[^\w\s\.]/g, "*");
+};
+
+type NumPosition = {
+      value: string;
+      position: number;
+}
+
+const calculateSum = (input: string): number => {
+  const allLines = input.split(/\r?\n/g);
+
+  let sum: number = 0;
+
+  for(let i:number = 0; i < allLines.length; i++) {) { 
+
+    let numStr: NumPosition[] = [];
+    let num: number = 0;
+
+    for(let j = 0; j < allLines[i].length; j++) {
+      if(allLines[i][j] !== "*") {
+        numStr.push({value: allLines[i][j], position: j});
+      } else if (numStr === "*" || numStr === "\n" || numStr === "\n") {
+
+      }
+
+    }
+
+  })
+
+  return 0;
+}
+
+
 export const main = (): number => {
   const fileData = readFile("input.txt");
+  const dataStars = replaceSpecialCharWithStars(fileData);
+  const finalSum = calculateSum(dataStars);
 
+  console.log(dataStars);
   console.log("The final sum is: "); //
 
   return 0;
