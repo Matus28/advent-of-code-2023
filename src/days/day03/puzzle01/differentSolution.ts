69748cf6837567e2b51bbe2bfd300db4bee4ddb0
Matus Tatarko <matus.tatarko28@gmail.com>
2023-12-06 21:49:25 +0100

    feat: solution for the day 3 puzzle 01

---
+++ src/days/day03/puzzle01/differentSolution.ts
@@ -0,0 +1,110 @@
+import { readFileSync } from "fs";
+import path from "path";
+
+interface Coord {
+  x: number;
+  y: number;
+}
+interface GridNode<T> {
+  value: T;
+  pos: Coord;
+}
+class Grid<T> {
+  data = new Map<string, GridNode<T>>();
+  add(pos: Coord, val: T) {
+    this.data.set(`${pos.x},${pos.y}`, { value: val, pos });
+  }
+  get(pos: Coord) {
+    this.data.get(`${pos.x},${pos.y}`);
+  }
+  forEach(cb: (v: GridNode<T>, k: string) => void) {
+    this.data.forEach(cb);
+  }
+}
+
+const boundary = (c: Coord, width: number) => ({
+  min: { x: c.x - 1, y: c.y - 1 },
+  max: { x: c.x + width, y: c.y + 1 },
+});
+const within = (c: Coord, bounds: { min: Coord; max: Coord }) =>
+  c.x >= bounds.min.x &&
+  c.y >= bounds.min.y &&
+  c.x <= bounds.max.x &&
+  c.y <= bounds.max.y;
+
+const inRange = (n: number, min: number, max: number) => n >= min && n <= max;
+const isDigit = (ch: string) =>
+  inRange(ch.charCodeAt(0) - "0".charCodeAt(0), 0, 9);
+
+function parseGrid(lines: string[]) {
+  const nums = new Grid<number>();
+  const symbols = new Grid<string>();
+  lines.forEach((line, y) => {
+    let numStr = "",
+      numX = -1;
+    for (let x = 0; x < line.length; x++) {
+      const ch = line.charAt(x);
+      if (ch === ".") {
+        // empty
+      } else if (isDigit(ch)) {
+        numStr += ch;
+        numX = numX === -1 ? x : numX;
+        if (x < line.length - 1) {
+          continue;
+        }
+      } else {
+        symbols.add({ x, y }, ch);
+      }
+
+      if (numStr.length > 0) {
+        nums.add({ x: numX, y }, parseInt(numStr));
+        numStr = "";
+        numX = -1;
+      }
+    }
+  });
+  return { nums, symbols };
+}
+
+function part1(lines: string[]) {
+  const { nums, symbols } = parseGrid(lines);
+  const result = findParts(nums, symbols).reduce((acc, cur) => acc + cur, 0);
+  console.log(`Part 1: ${result}`);
+}
+
+function findParts(nums: Grid<number>, symbols: Grid<string>) {
+  const parts: number[] = [];
+  nums.forEach((n) => {
+    symbols.forEach((s) => {
+      if (within(s.pos, boundary(n.pos, n.value.toString().length))) {
+        parts.push(n.value);
+      }
+    });
+  });
+  return parts;
+}
+
+function part2(lines: string[]) {
+  let result = 0;
+  const { nums, symbols } = parseGrid(lines);
+  const parts = findParts(nums, symbols);
+  symbols.forEach((s) => {
+    const adjacent: number[] = [];
+    nums.forEach((n) => {
+      if (parts.includes(n.value)) {
+        if (within(s.pos, boundary(n.pos, n.value.toString().length))) {
+          adjacent.push(n.value);
+        }
+      }
+    });
+    if (adjacent.length === 2) {
+      result += adjacent[0] * adjacent[1];
+    }
+  });
+  console.log(`Part 2: ${result}`);
+}
+const filePath = path.join(__dirname, "input.txt");
+
+const lines = readFileSync(filePath).toString().trim().split("\n");
+part1(lines);
+part2(lines);
--- src/days/day03/puzzle01/solution.ts
+++ src/days/day03/puzzle01/solution.ts
@@ -1,6 +1,19 @@
 import { readFileSync } from "fs";
 import path from "path";
 
+type NumPosition = {
+  value: string;
+  position: {
+    x: number;
+    y: number;
+  };
+};
+
+type CharPosition = {
+  x: number;
+  y: number;
+};
+
 const readFile = (filename: string): string => {
   const filePath = path.join(__dirname, filename);
   let content: string = "";
@@ -21,45 +34,107 @@ const replaceSpecialCharWithStars = (input: string): string => {
   return input.replace(/[^\w\s\.]/g, "*");
 };
 
-type NumPosition = {
-      value: string;
-      position: number;
-}
+const findSpecialCharPosition = (input: string[]): CharPosition[] => {
+  const specialCharPositions: CharPosition[] = [];
 
-const calculateSum = (input: string): number => {
-  const allLines = input.split(/\r?\n/g);
+  for (let i: number = 0; i < input.length; i++) {
+    for (let j: number = 0; j < input[i].length; j++) {
+      if (input[i][j] === "*") {
+        specialCharPositions.push({ x: i, y: j });
+      }
+    }
+  }
 
+  return specialCharPositions;
+};
+
+const verifyHasSpecialCharNext = (
+  numStr: NumPosition[],
+  charPos: CharPosition[]
+): boolean => {
+  let hasSpecialCharNext: boolean = false;
+  for (let i: number = 0; i < numStr.length; i++) {
+    for (let j: number = 0; j < charPos.length; j++) {
+      if (
+        numStr[i].position.x - charPos[j].x <= 1 &&
+        numStr[i].position.x - charPos[j].x >= -1 &&
+        numStr[i].position.y - charPos[j].y <= 1 &&
+        numStr[i].position.y - charPos[j].y >= -1
+      ) {
+        return true;
+      }
+    }
+  }
+
+  return hasSpecialCharNext;
+};
+
+const calculateSum = (allLines: string[], charPos: CharPosition[]): number => {
   let sum: number = 0;
 
-  for(let i:number = 0; i < allLines.length; i++) {) { 
-
+  for (let i: number = 0; i < allLines.length; i++) {
     let numStr: NumPosition[] = [];
-    let num: number = 0;
 
-    for(let j = 0; j < allLines[i].length; j++) {
-      if(allLines[i][j] !== "*") {
-        numStr.push({value: allLines[i][j], position: j});
-      } else if (numStr === "*" || numStr === "\n" || numStr === "\n") {
+    for (let j = 0; j < allLines[i].length; j++) {
+      if (
+        allLines[i][j] === "1" ||
+        allLines[i][j] === "2" ||
+        allLines[i][j] === "3" ||
+        allLines[i][j] === "4" ||
+        allLines[i][j] === "5" ||
+        allLines[i][j] === "6" ||
+        allLines[i][j] === "7" ||
+        allLines[i][j] === "8" ||
+        allLines[i][j] === "9"
+      ) {
+        numStr.push({ value: allLines[i][j], position: { x: i, y: j } });
+        if (j < allLines[i].length - 1) {
+          continue;
+        }
+      }
+      if (
+        (numStr.length > 0 &&
+          allLines[i][j] !== "1" &&
+          allLines[i][j] !== "2" &&
+          allLines[i][j] !== "3" &&
+          allLines[i][j] !== "4" &&
+          allLines[i][j] !== "5" &&
+          allLines[i][j] !== "6" &&
+          allLines[i][j] !== "7" &&
+          allLines[i][j] !== "8" &&
+          allLines[i][j] !== "9") ||
+        (numStr.length > 0 && j === allLines[i].length - 1)
+      ) {
+        const isValid = verifyHasSpecialCharNext(numStr, charPos);
+        if (isValid) {
+          let numStrValid: string = "";
+          for (let k: number = 0; k < numStr.length; k++) {
+            numStrValid += numStr[k].value;
+          }
 
+          sum += parseInt(numStrValid);
+        }
       }
 
+      numStr = [];
     }
+  }
 
-  })
-
-  return 0;
-}
-
+  return sum;
+};
 
 export const main = (): number => {
   const fileData = readFile("input.txt");
   const dataStars = replaceSpecialCharWithStars(fileData);
-  const finalSum = calculateSum(dataStars);
 
-  console.log(dataStars);
-  console.log("The final sum is: "); //
+  const linesArr = dataStars.split(/\r?\n/g);
 
-  return 0;
+  const specialCharPos = findSpecialCharPosition(linesArr); // const finalSum = calculateSum(dataStars);
+  const finalSum = calculateSum(linesArr, specialCharPos);
+
+  console.log("The final sum is: ", finalSum); // 560670
+
+  return finalSum;
 };
 
 main();
