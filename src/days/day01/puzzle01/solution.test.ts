30c571e4a993213f20710621e95ced6525d9ea59
Matus Tatarko <matus.tatarko28@gmail.com>
2023-12-07 21:57:25 +0100

    feat: Add jest configuration
    This commit deals with setting test enviroment
    * Install Jest package and dependencies
    * Create test file for the solution

---
+++ src/days/day01/puzzle01/solution.test.ts
@@ -0,0 +1,7 @@
+import { main } from "./solution";
+
+describe("solution module for day01-puzzle01", () => {
+  test("test if the solution - Calibration value is 54968", () => {
+    expect(main()).toBe(54968);
+  });
+});
